a3805d76a56fafa2f067160b085671308552cfea
dmas5 <artur.unt@gmail.com>
2024-07-18 02:30:06 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,130 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer, useState } from "react";
+
+
+const clientReducer = (clients,action) => {
+  switch (action.type) {
+    case 'addClient': {
+      return [...clients, 
+        {id: action.id, name: action.name, age: action.age}]
+    }
+    case 'deleteClient': {
+      return clients.filter(c => c.id !== action.id)
+    }
+    case 'edit': {
+      return clients.map(c => {
+        if(c.id === action.client.id){
+          return action.client
+        } else {
+          return c
+        }
+      })
+    }
+  }
+
+
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const generateId = () => Math.floor((Math.random() * 100000) + 1);
+
+  const [editedClientName,setEditedClientName] = useState('');
+  const [editedClientAge,setEditedClientAge] = useState();
+  const [editedClientId,setEditedClientId] = useState();
+
+  const [clients, dispatch] = useReducer(
+    clientReducer,
+    []
+  );
+
+  const handleAddClient = (event) => {
+
+    event.preventDefault();
+
+    dispatch({
+        type:'addClient',
+        id:generateId(),
+        name:event.target.clientName.value,
+        age:event.target.clientAge.value
+      });
+
+    event.target.clientName.value = '';
+    event.target.clientAge.value = '';
+
+  };
+
+  const handleDeleteClient = (id) => {
+
+    dispatch({type:'deleteClient', id:id});
+
+  };
+
+  const handleEditClient = (id) => {
+
+    const client = clients.find(c => c.id === id);
+    setEditedClientName(client.name);
+    setEditedClientAge(client.age);
+    setEditedClientId(client.id);
+
+  };
+
+  const submitEditClient = (event) => {
+
+    event.preventDefault();
+
+    let editedClient = {id:editedClientId, name:editedClientName, age:editedClientAge};
+    dispatch({type:'edit',client:editedClient});
+
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <div>
+      <p>Add new Client</p>
+      <form onSubmit={handleAddClient}>
+        <input name='clientName' />
+        <input name='clientAge' />
+        <button type='submit'>add</button>
+      </form>
+
+      <p>Edit Client</p>
+      <form onSubmit={submitEditClient}>
+        <input type='text' value={editedClientName} onChange={(e) => setEditedClientName(e.target.value)} />
+        <input type='text' value={editedClientAge} onChange={(e) => setEditedClientAge(e.target.value)} />
+        <button type='submit'>submit</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            clients.map((c,i) => {
+              return <tr key={i}>
+                <td>{c.id}</td>
+                <td>{c.name}</td>
+                <td>{c.age}</td>
+                <td>
+                  <button onClick={() => handleDeleteClient(c.id)}>delete</button>
+                </td>
+                <td>
+                  <button onClick={() => handleEditClient(c.id)}>edit</button>
+                </td>
+              </tr>
+            })
+          }
+        </tbody>
+      </table>
+
+
+
+    </div>
   )
 }
 
